82cd3fb042d5f2cb7dfef7fc65ffaa1d8d446479
DarkSoul7 <alejandro.tortolero7@gmail.com>
2016-03-02 20:24:10 +0100

    Corrección de errores

--- hids/functions/__init__.py
+++ hids/functions/__init__.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
-import pytz
-import tzlocal
-
-tz = str(tzlocal.get_localzone())
-t = datetime.utcnow()
-print(t)
-t.replace(tzinfo=pytz.timezone(str(tzlocal.get_localzone())))
-print(t.strftime('%Y-%m-%dT%H:%M'))
